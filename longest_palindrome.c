bea851439b557a2b236634b678dda197d2852c28
kar-ashishman <kar.ashishman@gmail.com>
2025-09-01 19:56:18 +0530

    Update longest_palindrome.c

--- longest_palindrome.c
+++ longest_palindrome.c
@@ -1,6 +1,7 @@
 /********
   Medium
   Given a string s, return the longest palindromic substring in s.
+  https://leetcode.com/problems/longest-palindromic-substring/
 *********/
 
 #include <string.h>
