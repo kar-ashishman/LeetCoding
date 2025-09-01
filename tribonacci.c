d71a93c3017697c2a3ebc53a05be9d1221fd00b9
kar-ashishman <kar.ashishman@gmail.com>
2025-09-01 19:56:54 +0530

    Update tribonacci.c

--- tribonacci.c
+++ tribonacci.c
@@ -1,33 +1,35 @@
-Link: https://leetcode.com/problems/n-th-tribonacci-number/description/
-Problem:
-Easy
-The Tribonacci sequence Tn is defined as follows: 
-
-T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
-
-Given n, return the value of Tn.
-
+/*
+ Link: https://leetcode.com/problems/n-th-tribonacci-number/description/
+ Problem:
+ Easy
+ The Tribonacci sequence Tn is defined as follows: 
  
-
-Example 1:
-
-Input: n = 4
-Output: 4
-Explanation:
-T_3 = 0 + 1 + 1 = 2
-T_4 = 1 + 1 + 2 = 4
-Example 2:
-
-Input: n = 25
-Output: 1389537
+ T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
  
-
-Constraints:
-
-0 <= n <= 37
-The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 - 1.
-
-Solution - C
+ Given n, return the value of Tn.
+ 
+  
+ 
+ Example 1:
+ 
+ Input: n = 4
+ Output: 4
+ Explanation:
+ T_3 = 0 + 1 + 1 = 2
+ T_4 = 1 + 1 + 2 = 4
+ Example 2:
+ 
+ Input: n = 25
+ Output: 1389537
+  
+ 
+ Constraints:
+ 
+ 0 <= n <= 37
+ The answer is guaranteed to fit within a 32-bit integer, ie. answer <= 2^31 - 1.
+ 
+ Solution - C
+*/
 int tribonacci(int n) {
     static int tribarr[38] = {0,1,1};
     if(n == 0) return 0;
