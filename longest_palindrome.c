a72d1f6254a87d7b9486edddc60cae1d64cd90aa
kar-ashishman <kar.ashishman@gmail.com>
2025-09-01 19:50:54 +0530

    Create longest_palindrome.c

---
+++ longest_palindrome.c
@@ -0,0 +1,51 @@
+/********
+  Medium
+  Given a string s, return the longest palindromic substring in s.
+*********/
+
+#include <string.h>
+#include <stdio.h>
+
+int checkpalin(char *ptr, int len);
+
+char* longestPalindrome(char* s) {
+    char* result;
+
+    int length = strlen(s);
+    if(length == 1) return s;
+    int res = 0, start = 0, len = length;
+
+    while(len >= 2) {
+        start = 0;
+        while((start + len) <= length) {
+            res = checkpalin(s + start, len);
+            if(res == 1) {
+                result = (char *) malloc(sizeof(char)*len + 1);
+                memcpy((void*)result, (s+start), len);
+                result[len] = 0;
+                return(result);
+            }
+            start ++;
+        }
+        len --;
+    }
+    result = (char *) malloc(2);
+    result[0] = *s;
+    result[1] = 0;
+    return result;
+}
+
+
+int checkpalin(char *ptr, int len) {
+    char *end = ptr + len-1;
+    int res = 1;
+    while(ptr < end) {
+        if(*ptr != *end) {
+            res = 0;
+            break;
+        }
+        ptr ++;
+        end --;
+    }
+    return res;
+}
